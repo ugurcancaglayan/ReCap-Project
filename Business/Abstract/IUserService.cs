7db77cb96c5edb612ccfc0b5af5cf6e677353aa0
Uğurcan Çağlayan <ugurcancaglayan@outlook.com>
2021-05-17 16:49:01 +0300

    Web API layer added.

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -9,7 +9,8 @@ namespace Business.Abstract
     public interface IUserService
     {
         IResult Add(User user);
-        IResult Delete(User user); IResult Update(User user); 
+        IResult Delete(User user); 
+        IResult Update(User user); 
         IDataResult<List<User>> GetAll(); 
         IDataResult<List<User>> GetById(int userId);
     }
